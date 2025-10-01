0f4f76f6399ae372e2408e80b1a4b2081596e307
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-01 14:38:53 +0530

    test

--- backend/src/webhook/github_webhook.py
+++ backend/src/webhook/github_webhook.py
@@ -21,7 +21,7 @@ def verify_signature(payload: Any, signature: str):
 
 @router.post("/webhook")
 async def github_webhook(request: Request, background_tasks: BackgroundTasks, x_hub_signature_256: Optional[str] = Header(None, alias="X-Hub-Signature-256"), x_github_event: Optional[str] = Header(None, alias="X-GitHub-Event")):
-    print(f"Webhook is triggered...")
+    print(f"New Webhook is triggered...")
     payload = await request.body()
     if not verify_signature(payload, x_hub_signature_256):
         raise HTTPException(status_code=401, detail="Invalid signature")
