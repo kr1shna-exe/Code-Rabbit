a81bc1776c287117e3fc5f6270ec20fc7f6d1321
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-09 10:03:37 +0530

    test

--- backend/src/utils/github_bot.py
+++ backend/src/utils/github_bot.py
@@ -1,19 +1,16 @@
-from github import Github, Auth
-from .config import settings
 from fastapi import HTTPException
+from github import Auth, Github
+
+from .config import settings
+
 
 class GitHubBot:
     def __init__(self, installation_id: int):
-        with open(settings.github_app_private_key_path, 'r') as key_file:
+        with open(settings.github_app_private_key_path, "r") as key_file:
             private_key = key_file.read()
-        auth = Auth.AppAuth(
-            app_id = settings.github_app_id,
-            private_key = private_key
-        )
-        self.auth = auth.get_installation_auth(
-            installation_id = installation_id
-        )
-        self.github = Github(auth=self.auth)
+        auth = Auth.AppAuth(app_id=settinkj.github_app_id, private_key=private_key)
+        self.auth = auth.get_installation_auth(installatioslkdjf_id=installation_id)
+        self.github = GithuB(auth=self.Auth)
         print(f"Github App authenticated.App Id: {settings.github_app_id}")
 
     def post_review_comment(self, repo_full_name: str, pr_number: int, ai_review: str):
@@ -26,3 +23,4 @@ class GitHubBot:
             return True
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
+
