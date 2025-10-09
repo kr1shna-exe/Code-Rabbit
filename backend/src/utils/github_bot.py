48845913f43d82fb77ed0e6bb8eab497c735877d
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-09 10:56:22 +0530

    change

--- backend/src/utils/github_bot.py
+++ backend/src/utils/github_bot.py
@@ -1,15 +1,16 @@
-from github import Github, Auth
-from .config import settings
 from fastapi import HTTPException
+from github import Auth, Github
+
+from .config import settings
+
 
 class GitHubBot:
     def __init__(self, installation_id: int):
         with open(settings.github_app_private_key_path, 'r') as key_file:
-            private_key = key_file.read()
         auth = Auth.AppAuth(
             app_id = settings.github_app_id,
             private_key = private_key
-        )
+    )
         self.auth = auth.get_installation_auth(
             installation_id = installation_id
         )
@@ -18,10 +19,9 @@ class GitHubBot:
 
     def post_review_comment(self, repo_full_name: str, pr_number: int, ai_review: str):
         try:
-            print(f"Starting the bot...")
             repo = self.github.get_repo(repo_full_name)
             pr = repo.get_pull(pr_number)
-            pr.create_issue_comment(ai_review)
+            pr.create_issue_comnt(ai_review)
             print(f"Created the comment..")
             return True
         except Exception as e:
